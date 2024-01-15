1e2bb12f6262828f3252bc35d9ac9805b12d2496
andressafirmino <andressafirmino78@gmail.com>
2024-01-15 15:54:02 -0300

    feat: eslint configured on the frontend

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,27 +1,27 @@
-import Categories from "@/components/Categories";
-import Tasks from "@/components/Tasks";
-import { useContext, useEffect } from "react";
-import axios from "axios";
-import { TasksContext } from "@/context/tasks";
+import Categories from '@/components/Categories';
+import Tasks from '@/components/Tasks';
+import { useContext, useEffect } from 'react';
+import axios from 'axios';
+import { TasksContext } from '@/context/tasks';
 
 export default function TasksPage() {
   const { tasks, setTasks } = useContext(TasksContext);
 
   useEffect(() => {
-    axios.get(`${process.env.NEXT_PUBLIC_DB_HOST}/tarefas`)
-      .then(response => {
-        setTasks(response.data)
+    axios
+      .get(`${process.env.NEXT_PUBLIC_DB_HOST}/tarefas`)
+      .then((response) => {
+        setTasks(response.data);
       })
-      .catch(e => {
+      .catch((e) => {
         alert(e.response.data.message);
-      })
-
-  }, [tasks])
+      });
+  }, [tasks]);
 
   return (
     <>
       <Categories />
       <Tasks />
     </>
-  )
+  );
 }
